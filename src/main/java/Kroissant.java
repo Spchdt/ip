dac540db7f45c92bd48c5ef0c21ff5e4b552223e
Supachod <spchdt@gmail.com>
2026-01-19 10:23:07 +0800

    feat: Add echos commands

--- src/main/java/Kroissant.java
+++ src/main/java/Kroissant.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Kroissant {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        Scanner scanner = new Scanner(System.in);
+
+        printLine();
         System.out.println("  Welcome! I'm Kroissant.");
         System.out.println("  What do you knead today?");
-        System.out.println("____________________________________________________________");
+        printLine();
+
+        String userMsg = "";
+        while(!userMsg.equals("bye")) {
+            System.out.println(userMsg);
+            System.out.print(">> ");
+            userMsg = scanner.nextLine();
+            printLine();
+        }
         System.out.println("  Time to roll out!");
         System.out.println("  Hope you have a loaf-ly day!");
+        printLine();
+    }
+
+    public static void printLine() {
         System.out.println("____________________________________________________________");
     }
 }
