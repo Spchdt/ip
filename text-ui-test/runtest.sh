939c7a2f877872166c8953db846ec7f3003c3bba
Supachod <spchdt@gmail.com>
2026-02-10 12:24:18 +0800

    chore: Fix test

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -12,19 +12,28 @@ then
     rm ACTUAL.TXT
 fi
 
+# delete tasks.ser from previous run
+if [ -e "./tasks.ser" ]
+then
+    rm tasks.ser
+fi
+
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/kroissant/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Kroissant < input.txt > ACTUAL.TXT
+java -classpath ../bin kroissant.Kroissant < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# use tr to remove carriage returns as dos2unix might not be available
+tr -d '\r' < ACTUAL.TXT > ACTUAL.tmp && mv ACTUAL.tmp ACTUAL.TXT
+tr -d '\r' < EXPECTED-UNIX.TXT > EXPECTED-UNIX.tmp && mv EXPECTED-UNIX.tmp EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
 diff ACTUAL.TXT EXPECTED-UNIX.TXT
